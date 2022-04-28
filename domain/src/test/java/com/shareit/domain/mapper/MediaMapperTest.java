e9fe5f47f750ad09b79634b8e18038b859f2926c
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 22:58:21 +0100

    test: making sure MediaMapperTest return properly the model and entity

--- domain/src/test/java/com/shareit/domain/mapper/MediaMapperTest.java
+++ domain/src/test/java/com/shareit/domain/mapper/MediaMapperTest.java
@@ -3,6 +3,7 @@ package com.shareit.domain.mapper;
 import com.shareit.domain.dto.Media;
 import com.shareit.domain.entity.MediaEntity;
 import com.shareit.domain.entity.MediaType;
+import com.shareit.infrastructure.upload.UploadedMedia;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
@@ -12,15 +13,34 @@ class MediaMapperTest {
 
     private final MediaMapper mediaMapper = Mappers.getMapper(MediaMapper.class);
 
+    private String fileName = "test.png";
+    private String url = "http://test.com/test.png";
+    private String publicId = "o1nx8";
+    private Long id = 1L;
 
-    private final MediaEntity mediaEntity = new MediaEntity(1L,
-            MediaType.IMAGE,
-            "test.png",
-            "123123");
+    private final MediaEntity mediaEntity = new MediaEntity(id, MediaType.IMAGE, url, publicId);
+    private final Media userModel = new Media(id, url, MediaType.IMAGE);
 
-    private final Media userModel = new Media(1L,
-            "test.png",
-            MediaType.IMAGE);
+    @Test
+    public void testToEntityImageUploadedMedia() {
+        MediaEntity entity = mediaMapper.toEntity(new UploadedMedia(fileName, url, publicId));
+        assertNotNull(entity);
+        assertEquals(new MediaEntity(null, MediaType.IMAGE, url, publicId), entity);
+    }
+
+    @Test
+    public void testToEntityVideoUploadedMedia() {
+        MediaEntity entity = mediaMapper.toEntity(new UploadedMedia("video.mp4", url, publicId));
+        assertNotNull(entity);
+        assertEquals(new MediaEntity(null, MediaType.VIDEO, url, publicId), entity);
+    }
+
+    @Test
+    public void testToEntityOtherUploadedMedia() {
+        MediaEntity entity = mediaMapper.toEntity(new UploadedMedia("document.doc", url, publicId));
+        assertNotNull(entity);
+        assertEquals(new MediaEntity(null, MediaType.OTHER, url, publicId), entity);
+    }
 
     @Test
     public void testToModel() {
