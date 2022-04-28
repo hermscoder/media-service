ac82f5b62610cfeed202b2f2d131ba60b081433d
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 21:52:40 +0100

    feat: adding custom message to InvalidParameterException

--- utils/src/main/java/com/shareit/utils/commons/exception/InvalidParameterException.java
+++ utils/src/main/java/com/shareit/utils/commons/exception/InvalidParameterException.java
@@ -4,7 +4,13 @@ public class InvalidParameterException extends BadRequestException {
     private String paramName;
 
     public InvalidParameterException(String paramName) {
-        super(String.format("Invalid param: %s", paramName));
+        this(paramName, null);
+    }
+
+    public InvalidParameterException(String paramName, String message) {
+        super(message == null
+                ? String.format("Invalid param: %s", paramName)
+                : String.format(message, paramName));
         this.paramName = paramName;
     }
 
