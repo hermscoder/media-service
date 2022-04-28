8774dc4c76102d60fde06c5793b120eca0b8dbdf
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 23:27:53 +0100

    feat:  adding hashCode and equals while we don't use lombok on the infrastructure layer

--- infrastructure/src/main/java/com/shareit/infrastructure/upload/UploadedMedia.java
+++ infrastructure/src/main/java/com/shareit/infrastructure/upload/UploadedMedia.java
@@ -1,5 +1,7 @@
 package com.shareit.infrastructure.upload;
 
+import java.util.Objects;
+
 public class UploadedMedia {
     private String fileName;
     private String url;
@@ -20,5 +22,27 @@ public class UploadedMedia {
 
     public String getPublicId() {
         return publicId;
+    }
+
+    @Override
+    public String toString() {
+        return "UploadedMedia{" +
+                "fileName='" + fileName + '\'' +
+                ", url='" + url + '\'' +
+                ", publicId='" + publicId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UploadedMedia that = (UploadedMedia) o;
+        return Objects.equals(fileName, that.fileName) && Objects.equals(url, that.url) && Objects.equals(publicId, that.publicId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileName, url, publicId);
     }
 }
