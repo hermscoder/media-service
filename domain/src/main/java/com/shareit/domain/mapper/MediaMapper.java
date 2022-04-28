42822cfbaeafcc0fcf3dd9d157316fd45112bfb3
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 22:57:29 +0100

    refactor: using mapstruct annotation instead of manual mapping

--- domain/src/main/java/com/shareit/domain/mapper/MediaMapper.java
+++ domain/src/main/java/com/shareit/domain/mapper/MediaMapper.java
@@ -5,14 +5,14 @@ import com.shareit.domain.entity.MediaEntity;
 import com.shareit.domain.entity.MediaType;
 import com.shareit.infrastructure.upload.UploadedMedia;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-//TODO unit tests
 @Mapper
 public interface MediaMapper {
     MediaMapper INSTANCE = Mappers.getMapper(MediaMapper.class);
@@ -21,13 +21,9 @@ public interface MediaMapper {
         return uploadedMediaList.stream().map(this::toEntity).collect(Collectors.toList());
     }
 
-    default MediaEntity toEntity(UploadedMedia uploadedMedia) {
-        return new MediaEntity(
-                null ,
-                MediaType.IMAGE,
-                uploadedMedia.getUrl(),
-                uploadedMedia.getPublicId());
-    }
+    @Mapping(target = "type", source = "fileName", qualifiedByName = "getMediaTypeByFileName")
+    @Mapping(target = "pendingDeletion", constant = "false")
+    MediaEntity toEntity(UploadedMedia uploadedMedia);
 
     default List<Media> toModeList(List<MediaEntity> mediaEntity) {
         return mediaEntity.stream().map(this::toModel).collect(Collectors.toList());
@@ -35,4 +31,21 @@ public interface MediaMapper {
 
     Media toModel(MediaEntity mediaEntity);
 
+    @Named("getMediaTypeByFileName")
+    default MediaType getMediaTypeByFileName(String fileName) {
+        String[] parts = fileName.split("\\.");
+        if(parts.length == 2) {
+            String extension = parts[1];
+
+            if(Arrays.asList("png", "jpg", "jpeg").contains(extension)) {
+                return MediaType.IMAGE;
+            } else if(Arrays.asList("mp4", "mov", "wmv", "avi", "webm", "html5").contains(extension)) {
+                return MediaType.VIDEO;
+            } else {
+                return MediaType.OTHER;
+            }
+
+        }
+        return null;
+    }
 }
