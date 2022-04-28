af4dcd247eaea12eea24ca50fad6bff4c53cbe51
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 21:55:05 +0100

    test: making sure create media works, and it throws exceptions that are being throw by the dependencies.

--- business/src/test/java/com/shareit/business/upload/MediaServiceTest.java
+++ business/src/test/java/com/shareit/business/upload/MediaServiceTest.java
@@ -5,12 +5,17 @@ import com.shareit.data.repository.MediaRepository;
 import com.shareit.domain.dto.Media;
 import com.shareit.domain.entity.MediaEntity;
 import com.shareit.domain.entity.MediaType;
+import com.shareit.infrastructure.upload.UploadError;
+import com.shareit.infrastructure.upload.UploadedMedia;
 import com.shareit.infrastructure.upload.UploaderService;
+import com.shareit.infrastructure.upload.exception.FailedUploadException;
+import com.shareit.utils.commons.exception.InvalidParameterException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,13 +29,22 @@ class MediaServiceTest {
     private final MediaService mediaService;
     private final MediaRepository mediaRepository;
 
+    private String fileName = "test.png";
+    private String url = "http://test.com/test.png";
+    private String publicId = "o1nx8";
+    private Long id = 1L;
 
-    private MediaEntity mediaEntity = new MediaEntity(1L,
-            MediaType.VIDEO,
-            "https://uploadedurl.com",
-            "m29fvh6s");
 
-    private Media expectedMedia = new Media(1L, "test.png", MediaType.IMAGE);
+
+    private List<UploadedMedia> uploadedMediaList = Arrays.asList(new UploadedMedia(fileName, url, publicId));
+
+    private List<MediaEntity> mediaEntityList = Arrays.asList(
+            new MediaEntity(id,
+                    MediaType.IMAGE,
+                    url,
+                    publicId));
+
+    private Media expectedMedia = new Media(id, url, MediaType.IMAGE);
 
     public MediaServiceTest() {
         this.uploaderService = Mockito.mock(UploaderService.class);
@@ -40,15 +54,52 @@ class MediaServiceTest {
 
     @Test
     public void testCreateMedia() {
-        when(mediaRepository.save(any(MediaEntity.class))).thenReturn(mediaEntity);
+        when(uploaderService.uploadParallel(any(MultipartFile[].class), anyMap())).thenReturn(uploadedMediaList);
+        when(mediaRepository.saveAll(anyList())).thenReturn(mediaEntityList);
 
-        MultipartFile[] multipartFiles = new MultipartFile[] {new MockMultipartFile("test.png", new byte[10])};
+        MultipartFile[] multipartFiles = new MultipartFile[] {new MockMultipartFile(fileName, new byte[10])};
 
         List<Media> media = mediaService.createMedias(multipartFiles);
         assertNotNull(media);
         assertFalse(media.isEmpty());
-        assertEquals(expectedMedia.getId(), media.get(0).getId());
-        verify(uploaderService).upload(multipartFiles[0], Collections.emptyMap());
+        assertEquals(expectedMedia, media.get(0));
+        verify(uploaderService).uploadParallel(multipartFiles, Collections.emptyMap());
 
+    }
+
+    @Test
+    public void testCreateMediaThrowBadRequestException() {
+        when(uploaderService.uploadParallel(any(MultipartFile[].class), anyMap())).thenReturn(uploadedMediaList);
+        when(mediaRepository.saveAll(anyList())).thenReturn(mediaEntityList);
+
+        InvalidParameterException badRequestException = assertThrows(InvalidParameterException.class, () -> mediaService.createMedias(new MultipartFile[0]));
+        assertNotNull(badRequestException);
+        assertEquals("Unable to upload. No files were provided.", badRequestException.getMessage());
+    }
+
+    @Test
+    public void testCreateMediaThrowInvalidParameterExceptionByUploader() {
+        doThrow(new InvalidParameterException("files", "Unable to upload. An empty file (test.png) was provided.")).when(uploaderService).uploadParallel(any(MultipartFile[].class), anyMap());
+        when(mediaRepository.saveAll(anyList())).thenReturn(mediaEntityList);
+
+        InvalidParameterException invalidParameterException = assertThrows(InvalidParameterException.class, () -> mediaService.createMedias(new MultipartFile[0]));
+        assertNotNull(invalidParameterException);
+        assertEquals("Unable to upload. No files were provided.", invalidParameterException.getMessage());
+        assertEquals("files", invalidParameterException.getParamName());
+    }
+
+    @Test
+    public void testCreateMediaThrowFailedUploadExceptionByUploader() {
+        List<UploadError> expectedUploadErrorList = Arrays.asList(new UploadError("test.png", "Max file size exceeded"));
+        doThrow(new FailedUploadException("Upload failed", expectedUploadErrorList)).when(uploaderService).uploadParallel(any(MultipartFile[].class), anyMap());
+        when(mediaRepository.saveAll(anyList())).thenReturn(mediaEntityList);
+
+        FailedUploadException failedUploadException =
+                assertThrows(
+                        FailedUploadException.class,
+                        () -> mediaService.createMedias(new MultipartFile[] {new MockMultipartFile(fileName, new byte[10])}));
+        assertNotNull(failedUploadException);
+        assertEquals("Upload failed", failedUploadException.getMessage());
+        assertEquals(expectedUploadErrorList.get(0), failedUploadException.getUploadErrors().get(0));
     }
 }
