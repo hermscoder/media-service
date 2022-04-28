06e5fcdeb30fb515df2439dcd9ad254ec6fc7d42
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-28 21:54:30 +0100

    refactor: adding exception to method signature

--- infrastructure/src/main/java/com/shareit/infrastructure/upload/UploaderService.java
+++ infrastructure/src/main/java/com/shareit/infrastructure/upload/UploaderService.java
@@ -2,6 +2,8 @@ package com.shareit.infrastructure.upload;
 
 import com.shareit.infrastructure.upload.exception.FailedDestructionException;
 import com.shareit.infrastructure.upload.exception.FailedUploadException;
+import com.shareit.utils.commons.exception.BadRequestException;
+import com.shareit.utils.commons.exception.InvalidParameterException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -10,5 +12,5 @@ import java.util.Map;
 public interface UploaderService {
     Map upload(MultipartFile multipartFile, Map options) throws FailedUploadException;
     Map destroy(String publicId, Map options) throws FailedDestructionException;
-    public List<UploadedMedia> uploadParallel(MultipartFile[] multipartFiles, Map options) throws FailedUploadException;
+    List<UploadedMedia> uploadParallel(MultipartFile[] multipartFiles, Map options) throws FailedUploadException, InvalidParameterException;
 }
