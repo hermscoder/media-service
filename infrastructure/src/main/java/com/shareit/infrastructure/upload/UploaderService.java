2047bac8a3a2449111b3edaebfda9d1dbe547a50
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-11 23:21:30 +0100

    refactor: changing return of methods and adding new destroyUploadedMedias

--- infrastructure/src/main/java/com/shareit/infrastructure/upload/UploaderService.java
+++ infrastructure/src/main/java/com/shareit/infrastructure/upload/UploaderService.java
@@ -7,9 +7,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public interface UploaderService {
-    Map upload(MultipartFile multipartFile, Map options) throws FailedUploadException;
+    UploadedMedia upload(MultipartFile multipartFile, Map options) throws FailedUploadException;
     Map destroy(String publicId, Map options) throws FailedDestructionException;
     List<UploadedMedia> uploadParallel(MultipartFile[] multipartFiles, Map options) throws FailedUploadException, InvalidParameterException;
+    CompletableFuture<Void> destroyUploadedMedias(List<String> publicIds);
 }
