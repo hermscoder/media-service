9d8e17ea52596dcd7d5a0106260df6e1338da71d
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-18 22:52:02 +0100

    refactor: removing unnecessary imports

--- infrastructure/src/test/java/com/shareit/infrastructure/upload/FileUploaderServiceTest.java
+++ infrastructure/src/test/java/com/shareit/infrastructure/upload/FileUploaderServiceTest.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
