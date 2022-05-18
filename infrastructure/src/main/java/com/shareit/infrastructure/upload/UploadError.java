9b8d96d5dd50965959b379fc3eb70090e2f96ff5
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-18 21:18:34 +0100

    feat: implementing equals,hashcode and toString tot UploadError

--- infrastructure/src/main/java/com/shareit/infrastructure/upload/UploadError.java
+++ infrastructure/src/main/java/com/shareit/infrastructure/upload/UploadError.java
@@ -1,5 +1,7 @@
 package com.shareit.infrastructure.upload;
 
+import java.util.Objects;
+
 public class UploadError {
     private String fileName;
     private String errorMsg;
@@ -15,5 +17,26 @@ public class UploadError {
 
     public String getErrorMsg() {
         return errorMsg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UploadError that = (UploadError) o;
+        return Objects.equals(fileName, that.fileName) && Objects.equals(errorMsg, that.errorMsg);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileName, errorMsg);
+    }
+
+    @Override
+    public String toString() {
+        return "UploadError{" +
+                "fileName='" + fileName + '\'' +
+                ", errorMsg='" + errorMsg + '\'' +
+                '}';
     }
 }
